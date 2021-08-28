be406104b39f0e40affe387fca5e0834644847b6
Ilya Korolev <ilya_korolev@zoho.eu>
2021-08-28 10:19:23 +0300

    implement AstroRefreshIndicator

--- lib/astro_refresher.dart
+++ lib/astro_refresher.dart
@@ -1,7 +1,3 @@
 library astro_refresher;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export './src/astro_refresh_indicator.dart';
---
+++ lib/src/astro_refresh_indicator.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/cupertino.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:rive/rive.dart';
+
+class AstroRefreshIndicator extends RefreshIndicator {
+  const AstroRefreshIndicator({
+    this.fit = BoxFit.cover,
+    this.backgroundColor = const Color(0xFF43378D),
+    Duration completeDuration = const Duration(milliseconds: 500),
+    double height = 150,
+    RefreshStyle refreshStyle = RefreshStyle.UnFollow,
+    Key? key,
+  }) : super(
+          key: key,
+          height: height,
+          completeDuration: completeDuration,
+          refreshStyle: refreshStyle,
+        );
+
+  final BoxFit fit;
+  final Color backgroundColor;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _AstroRefreshIndicatorState();
+  }
+}
+
+class _AstroRefreshIndicatorState extends RefreshIndicatorState<AstroRefreshIndicator> {
+  late final Artboard _riveArtboard;
+  late final StateMachineController _riveController;
+  late final SMIInput<double> _pullAmountInput;
+  late final SMIInput<bool> _isLoadingInput;
+
+  bool _assetLoaded = false;
+  double _innerHeight = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _initRive();
+  }
+
+  Future<void> _initRive() async {
+    final file = await RiveFile.asset('packages/astro_refresher/assets/space_reload.riv');
+
+    _riveArtboard = file.mainArtboard;
+    _riveController = StateMachineController.fromArtboard(_riveArtboard, 'Reload')!;
+
+    _riveArtboard.addController(_riveController);
+
+    _pullAmountInput = _riveController.findInput<double>('Pull Amount')!;
+    _isLoadingInput = _riveController.findInput<bool>('Is Loading')!;
+
+    setState(() => _assetLoaded = true);
+  }
+
+  @override
+  void onOffsetChange(double offset) {
+    if (!_assetLoaded) {
+      return;
+    }
+
+    if (offset < 0.0 || offset > widget.height) {
+      return;
+    }
+
+    _updatePullAmount(offset);
+
+    if (mode == RefreshStatus.idle) {
+      setState(() => _innerHeight = offset);
+    }
+  }
+
+  void _updatePullAmount(double offset) {
+    if (_isLoadingInput.value) {
+      return;
+    }
+
+    final pullAmount = 100.0 * offset / widget.height;
+
+    _pullAmountInput.value = pullAmount;
+  }
+
+  @override
+  void onModeChange(RefreshStatus? mode) {
+    if (!_assetLoaded) {
+      return;
+    }
+
+    if (mode == RefreshStatus.canRefresh) {
+      _isLoadingInput.value = true;
+    }
+
+    if (mode == RefreshStatus.completed || mode == RefreshStatus.idle) {
+      _isLoadingInput.value = false;
+    }
+  }
+
+  @override
+  Widget buildContent(BuildContext context, RefreshStatus mode) {
+    if (!_assetLoaded) {
+      return const SizedBox();
+    }
+
+    return Container(
+      height: _innerHeight,
+      color: widget.backgroundColor,
+      child: Rive(
+        artboard: _riveArtboard,
+        fit: widget.fit,
+      ),
+    );
+  }
+}
